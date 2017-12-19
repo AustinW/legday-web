c93a0dfbe838409a08e9958b3bd55ef427c00c57
Austin White <austingym@gmail.com>
2017-12-18 16:14:55 -0800

    Added new section. Incorporated grunt into the build process including less stylesheet.

---
+++ gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/main.js',
+        dest: 'dist/main.min.js'
+      }
+    },
+    less: {
+      development: {
+        options: {
+          paths: ['dist/css']
+        },
+        files: {
+          'dist/style.css': 'src/less/main.less'
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['src/less/main.less', 'src/index.html', 'src/main.js', 'gruntfile.js'],
+        tasks: ['default'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+    copy: {
+      main: {
+        files: [
+          // includes files within path
+          {expand: false, src: ['src/index.html'], dest: 'dist/index.html'}
+        ],
+      },
+    },
+    php: {
+      dev: {
+        options: {
+          port: 8080
+          
+        }
+      },
+      watch: {}
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-php');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'less', 'copy']);
+  grunt.registerTask('start', ['default', 'php:dev', 'watch'])
+
+};
